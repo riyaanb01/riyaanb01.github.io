6ee97cb8f6698aef19c2496f14fafa489f5297b2
riyaanb01 <nradia01@gmail.com>
2025-04-15 17:08:34 -0400

    Update

--- random-joke-generator/script.js
+++ random-joke-generator/script.js
@@ -1,26 +1,14 @@
-function myfunction(){
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
-    let calculate = document.getElementById("calculate").value;
-
-    if(calculate==="+"){
-    let num1 = parseInt( document.getElementById("num1").value);
-    let num2 = parseInt (document.getElementById("num2").value);
-    document.getElementById("result").value=num1+num2;
-    }
-if(calculate==="-"){
-    let num1 = parseInt( document.getElementById("num1").value);
-    let num2 = parseInt (document.getElementById("num2").value);
-    document.getElementById("result").value=num1-num2;
-}
-    if(calculate==="*"){
-    let num1 = parseInt( document.getElementById("num1").value);
-    let num2 = parseInt (document.getElementById("num2").value);
-    document.getElementById("result").value=num1*num2;
-    }
-    if(calculate==="/"){
-    let num1 = parseInt( document.getElementById("num1").value);
-    let num2 = parseInt (document.getElementById("num2").value);
-    document.getElementById("result").value=num1/num2;
-    }
-}
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+let getJoke = () => {
+    jokeContainer.classList.remove("fade");
+    fetch(url)
+        .then((data) => data.json())
+        .then((item) => {
+            jokeContainer.textContent = `${item.joke}`;
+            jokeContainer.classList.add("fade");
+        });
+};
+btn.addEventListener("click", getJoke);
+getJoke();
