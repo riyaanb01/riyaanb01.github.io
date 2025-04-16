7faa2d349b2b26cea564b64bbd69fb241aace990
riyaanb01 <nradia01@gmail.com>
2025-04-16 10:33:26 -0400

    New Project

---
+++ background-changer/script.js
@@ -0,0 +1,23 @@
+let currentColor = document.querySelector('.current-color');
+let bodyElem = document.querySelector('body');
+let generateBtn = document.querySelector('.generate-btn');
+
+bodyElem.style.backgroundColor = currentColor.innerText;
+
+generateBtn.addEventListener('click', () => {
+    let randomColor = "";
+    let charactors = "0123456789abcdef";
+    /*
+    The for loop is very useful in Javascript syntax:
+    for (initializer; condition; increment) {
+        code
+    }
+    */
+
+    for (let i = 0; i < 6; i++) {
+        randomColor = randomColor + charactors[Math.floor(Math.random() * 16)];
+    }
+
+    currentColor.innerText = "#" + randomColor;
+    bodyElem.style.backgroundColor = "#" + randomColor;
+})
